d500324756fe34a9258e7bf8b903d1d122ed6dde
ich95 <56219402+ich95@users.noreply.github.com>
2020-02-12 07:58:21 +0100

    Update TicketAbfrage.py
    if username/password wrong -> abort after 3 retries

--- TicketAbfrage.py
+++ TicketAbfrage.py
@@ -1,13 +1,15 @@
-
 import requests
 from getpass import getpass
 import re
 import time
 
 name = input("Name: ")
-
 pwd = getpass()
+delay = 10  # Zeit zwischen abfragen in Sekunden
+
 run = 1
+count = 0
+
 while True:
     print(run)
     run += 1
@@ -15,15 +17,19 @@ while True:
     url = "https://wlan-login.oszimt.de/logon/cgi/index.cgi"
     req = requests.post(url, allow_redirects=False, data={})
     htmlAntwort = req.text  # HTML Antowrt der Seite in Variable speichern
-    x = re.findall('\"[\d\D]{32}\s[\d\D]{32}\"', htmlAntwort) #ta_id hat 2 32 Bit Hex-Felder, die durch Leerzeichen getrennt sind, " sorgt für genau diesen gesuchten Text ohne andere Werte
-    print(x) # Ausgabe des Arrays von re.findall
-    if(len(x) == 0): #Wenn Array leer -> Ticket noch gültig -> muss nicht neu anmelden
-        time.sleep(10) #Zeit, die zwischen Abfragen liegen soll
+    # ta_id hat 2 32 Bit Hex-Felder, die durch Leerzeichen getrennt sind, " sorgt für genau diesen gesuchten Text ohne andere Werte
+    x = re.findall('\"[\d\D]{32}\s[\d\D]{32}\"', htmlAntwort) 
+    # print(x)  # Ausgabe des Arrays von re.findall (Debug)
+    if(len(x) == 0):  # Wenn Array leer -> Ticket noch gültig -> muss nicht neu anmelden
+        time.sleep(delay)  # Zeit, die zwischen Abfragen liegen soll
+        count = 0  # count wieder auf null setzen, da sonst nach 3 reconnects sonst Skript abbricht
         continue
-    else: # Neuanmeldung initiieren
-        x[0] = x[0].replace('"', "") #wenn Array Wert enthält, dann nur mit "Code" -> für senden müssen Daten ohne " sein
-    print(x) #ausgabe des Arrays, nur zur Kontrolle
-    # print(htmlAntwort)
+    else:  # Neuanmeldung initiieren
+        # wenn Array Wert enthält, dann nur mit "Code" -> für senden müssen Daten ohne " sein
+        x[0] = x[0].replace('"', "")
+    print(x)  # ausgabe des Arrays (Debug)
+    # print(htmlAntwort) #Ausgabe der Antwort (HTML Volltext) (Debug)
+    # request an Action-Seite im Login Formular
     url = "https://wlan-login.oszimt.de/logon/cgi/index.cgi#anchor_voucherLogon"
     req = requests.post(url, allow_redirects=False, data={
         'uid': name,
@@ -31,4 +37,11 @@ while True:
         'ta_id': x[0],
         'voucher_logon_btn': True
     })
-    
+
+    if len(x) != 0:
+        count += 1
+
+    if count > 2:
+        print("Nutzername oder Passwort überprüfen")
+        break
+
